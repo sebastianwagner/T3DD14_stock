2be603668d76d843c57840fd02c27ae65a8bf375
Sebastian Wagner <2000sw@gmail.com>
2014-06-21 11:43:39 +0200

    custom model with testcase

---
+++ Classes/Domain/Model/Product.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace T3DD14\Stock\Domain\Model\Product;
+use TYPO3\CMS\Extbase\DomainObject\AbstractDomainObject;
+
+class Product extends AbstractDomainObject {
+
+}
---
+++ Tests/Unit/Domain/Model/ProductTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace T3DD14\Stock\Tests\Unit\Domain\Model;
+
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+use T3DD14\Stock\Domain\Model\Product;
+
+/**
+ * Class ProductTest
+ * test case
+ *
+ * @package T3DD14\Stock\Tests\Unit\Domain\Model
+ */
+class ProductTest extends UnitTestCase {
+
+}
