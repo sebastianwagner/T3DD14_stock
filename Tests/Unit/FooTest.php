cc2cb38a1c5586c1121195cace18089b91399f5a
Sebastian Wagner <2000sw@gmail.com>
2014-06-21 11:19:01 +0200

    dummy unit test

---
+++ Tests/Unit/FooTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace T3DD14\Stock\Tests\Unit;
+
+/**
+ * Class FooTest
+ * @package T3DD14\Stock\Tests\Unit
+ */
+class FooTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+    /**
+     * @test
+     */
+    public function universeWorks() {
+        $this->assertSame(
+            1 + 1,
+            2
+        );
+    }
+}
